9d3c1028722e1e5dd0648ede2733fd9cbce9b458
Gareth Down <g@garethdown.com>
2019-08-28 07:45:58 +0200

    init

---
+++ config.js
@@ -0,0 +1,45 @@
+const config = {
+	"gatsby": {
+		"pathPrefix": "/",
+		"siteUrl": "https://gitbook.io/mocksome",
+		"gaTrackingId": null
+	},
+	"header": {
+		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
+		"logoLink": "https://gitbook.io/mocksome",
+		"title": "Mocksome Documentation",
+		"githubUrl": "https://github.com/mocksome",
+		"helpUrl": "",
+		"tweetText": "",
+		"links": [
+			{ "text": "", "link": ""}
+		],
+		"search": {
+			"enabled": false,
+			"indexName": "",
+			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
+			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
+		}
+	},
+	"sidebar": {
+		"forcedNavOrder": [
+			"/introduction",
+    		"/codeblock"
+		],
+		"links": [
+			{ "text": "Hasura", "link": "https://hasura.io"},
+		],
+		"frontline": false,
+		"ignoreIndex": true,
+	},
+	"siteMetadata": {
+		"title": "Mocksome",
+		"description": "Documentation built with mdx. Powering mocksome.com",
+		"ogImage": null,
+		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
+		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
+	},
+};
+
+module.exports = config;
