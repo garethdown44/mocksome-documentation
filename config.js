49300cecc73841ea6de73357fb21a9acdec3f473
Gareth Down <g@garethdown.com>
2020-01-30 07:30:12 +0000

    improve layout and add GA tracking code

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const config = {
 	"gatsby": {
 		"pathPrefix": "/",
-		"siteUrl": "https://gitbook.io/mocksome",
+		"siteUrl": "https://docs.mocksome.com",
 		"gaTrackingId": null
 	},
 	"header": {
-		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
-		"logoLink": "https://gitbook.io/mocksome",
+		"logo": "",
+		"logoLink": "https://www.mocksome.com",
 		"title": "Mocksome Documentation",
 		"githubUrl": "https://github.com/mocksome",
 		"helpUrl": "",
@@ -15,27 +15,27 @@ const config = {
 			{ "text": "", "link": ""}
 		],
 		"search": {
-			"enabled": false,
-			"indexName": "",
-			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
-			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
+			"enabled": true,
+			"indexName": "mocksome-docs", 
+			"algoliaAppId": 'DBQGSCFP0B', // process.env.GATSBY_ALGOLIA_APP_ID,
+			"algoliaSearchKey": 'a4d4f42a5d071042e1e0c3dc764db03c', //process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+			"algoliaAdminKey": '41ba9dad939e8c6e59bfee9db4d657eb', //process.env.ALGOLIA_ADMIN_KEY
 		}
 	},
 	"sidebar": {
 		"forcedNavOrder": [
 			"/introduction",
-    		"/codeblock"
+    		"/api"
 		],
 		"links": [
-			{ "text": "Hasura", "link": "https://hasura.io"},
+			// { "text": "Hasura", "link": "https://hasura.io"},
 		],
 		"frontline": false,
 		"ignoreIndex": true,
 	},
 	"siteMetadata": {
 		"title": "Mocksome",
-		"description": "Documentation built with mdx. Powering mocksome.com",
+		"description": "Mocksome. Documentation site for Mocksome HTTP Mocking Server",
 		"ogImage": null,
 		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
 		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
